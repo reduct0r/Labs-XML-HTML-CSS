d5b575d6ef5bad02db238a282bd5c93584a9f9ce
reduct0r <yanshinti@student.bmstu.ru>
2025-05-22 04:33:15 +0300

    CORS fix

--- lab4/src/main.ts
+++ lab4/src/main.ts
@@ -6,6 +6,12 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // Разрешаем запросы с этих источников
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Разрешённые методы
+    allowedHeaders: ['Content-Type'], // Разрешённые заголовки
+  });
+
   const publicPath = join(process.cwd(), 'public');
   console.log('Serving static files from:', publicPath);
   app.useStaticAssets(publicPath);
